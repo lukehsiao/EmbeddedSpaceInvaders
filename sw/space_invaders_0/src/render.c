f0ba3a4e3229e0567d8b9ff067652b062e395c20
Luke Hsiao <luke.w.hsiao@gmail.com>
2014-09-23 12:51:44 -0600

    Added empty render files.

---
+++ sw/space_invaders_0/src/render.c
@@ -0,0 +1,12 @@
+/**
+ * This implements render.h
+ * @author Luke Hsiao
+ * @author Jeff Ravert
+ * @date 23 Sept 2014
+ */
+
+#include "render.h"
+
+void render() {
+
+}
---
+++ sw/space_invaders_0/src/render.h
@@ -0,0 +1,15 @@
+/**
+ * This interface will provide a structured way to handle rendering
+ *
+ * @author Luke Hsiao
+ * @author Jeff Ravert
+ * @date 23 Sept 2014
+ */
+
+#ifndef RENDER_H_
+#define RENDER_H_
+
+
+void render();
+
+#endif /* RENDER_H_ */
