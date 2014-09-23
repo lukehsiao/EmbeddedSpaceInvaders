50136e70e226f0caf513ec5483fdc2609236e82f
Luke Hsiao <luke.w.hsiao@gmail.com>
2014-09-23 13:59:59 -0600

    Small edits.  Need to answer how to remember old positions.

--- sw/space_invaders_0/src/render.c
+++ sw/space_invaders_0/src/render.c
@@ -5,12 +5,13 @@
  * @date 23 Sept 2014
  */
 
+#define FRAME_BUFFER_ADDR 0xC0000000
 #include "render.h"
 #include "globals.h"		//to give access to the global variables
 
-#define FRAME_BUFFER_ADDR 0xC0000000
-
-
+/**
+ * Draws the tank in it's current position
+ */
 void renderTank() {
 	unsigned int* framePointer0 = (unsigned int *) FRAME_BUFFER_ADDR;
 
@@ -23,30 +24,34 @@ void renderTank() {
 	}
 }
 
+void eraseTank() {
+
+}
+
 void render() {
+	unsigned int* framePointer0 = (unsigned int *) FRAME_BUFFER_ADDR;
+	int row=0, col=0;
+	for( row=0; row<480; row++) {
+		for(col=0; col<640; col++) {
+			if(row < 240) {
+				if(col<320) {
+					// upper left corner.
+					framePointer0[row*640 + col] = 0x00000000;  // frame 0 is red here.
+				} else {
+					// upper right corner.
+					framePointer0[row*640 + col] = 0x00000000;  // frame 0 is blue here.
+				}
+			} else {
+				if(col<320) {
+					// lower left corner.
+					framePointer0[row*640 + col] = 0x00000000;  // frame 0 is green here.
+				} else {
+					// lower right corner.
+					framePointer0[row*640 + col] = 0x00000000;  // frame 0 is white here.
+				}
+			}
+		}
+	}
 	renderTank();
-//	unsigned int* framePointer0 = (unsigned int *) FRAME_BUFFER_ADDR;
-//	int row=0, col=0;
-//	for( row=0; row<480; row++) {
-//		for(col=0; col<640; col++) {
-//			if(row < 240) {
-//				if(col<320) {
-//					// upper left corner.
-//					framePointer0[row*640 + col] = 0x00000000;  // frame 0 is red here.
-//				} else {
-//					// upper right corner.
-//					framePointer0[row*640 + col] = 0x00000000;  // frame 0 is blue here.
-//				}
-//			} else {
-//				if(col<320) {
-//					// lower left corner.
-//					framePointer0[row*640 + col] = 0x00000000;  // frame 0 is green here.
-//				} else {
-//					// lower right corner.
-//					framePointer0[row*640 + col] = 0x00000000;  // frame 0 is white here.
-//				}
-//			}
-//		}
-//	}
 }
 
