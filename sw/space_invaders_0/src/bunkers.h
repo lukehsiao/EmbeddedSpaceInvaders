66b160c6f42e4b482b703b484abbe0ee6c9144bd
lukehsiao <luke.w.hsiao@gmail.com>
2014-10-17 22:37:32 +0000

    bunkers.h edited online with Bitbucket

--- sw/space_invaders_0/src/bunkers.h
+++ sw/space_invaders_0/src/bunkers.h
@@ -10,7 +10,19 @@
 #ifndef BUNKERS_H_
 #define BUNKERS_H_
 
+/**
+ * Draws the bunker
+ * @param bunkerNumber The bunker to draw (0-3)
+ */
 void renderBunker(u8 bunkerNumber);
+
+/**
+ * Calculates whether the given position falls within the bunker.
+ *
+ * @param position the (x,y) position to test
+ * @param bunkerNumber the number of the bunker to test
+ * @return the block number that the position falls in, or 0xFF if it isn't hit
+ */
 u8 hitBunker(point_t position, u8 bunkerNumber);
 
 #endif /* BUNKERS_H_ */
