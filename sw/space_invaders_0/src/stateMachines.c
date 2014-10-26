111b2fd83d8b241b9cfc93e54239fb97f8017ccf
Luke Hsiao <luke.w.hsiao@gmail.com>
2014-10-25 18:20:50 -0600

    Added polling sound loop test.

--- sw/space_invaders_0/src/stateMachines.c
+++ sw/space_invaders_0/src/stateMachines.c
@@ -21,6 +21,8 @@
 #include "mb_interface.h"   // provides the microblaze interrupt enables, etc.
 #include "xintc_l.h"        // Provides handy macros for the interrupt controller.
 
+task tasks[TASKS_NUM];
+
 /////////////////////////////////////
 // Handy Timing Functions
 /////////////////////////////////////
