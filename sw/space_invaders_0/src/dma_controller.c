870f19719e1ec965bdbc353fbab4e9ad6c89fad7
lukehsiao <luke.w.hsiao@gmail.com>
2014-12-10 23:24:35 +0000

    dma_controller.c edited online with Bitbucket

--- sw/space_invaders_0/src/dma_controller.c
+++ sw/space_invaders_0/src/dma_controller.c
@@ -14,67 +14,22 @@
 
 
 /**
- *
- * User logic master module to send/receive word to/from remote system memory.
- * While sending, one word is read from user logic local FIFO and written to remote system memory.
- * While receiving, one word is read from remote system memory and written to user logic local FIFO.
+ * User logic master module to copy data from one location in memory to another.
+ * Because the hardware uses the AXI4-Lite protocol, one word is read from src
+ * and written to destination at a time. This process is then repeated for
+ * 'Length' words.
  *
  * @param   BaseAddress is the base address of the DMA_CONTROLLER device.
- * @param   Src/DstAddress is the destination system memory address from/to which the data will be fetched/stored.
- *
- * @return  None.
- *
- * @note    None.
- *
+ * @param   SrcBaseAddr is the base address of where to copy from.
+ * @param   DestBaseAddr is the base address of where to copy to.
+ * @param   Length is the number of words to transfer (should be > 0).
  */
-//void DMA_CONTROLLER_MasterSendWord(Xuint32 BaseAddress, Xuint32 DstAddress)
-//{
-//  /*
-//   * Set user logic master control register for write transfer.
-//   */
-//  Xil_Out8(BaseAddress+DMA_CONTROLLER_MST_CNTL_REG_OFFSET, MST_SGWR);
-//
-//  /*
-//   * Set user logic master address register to drive IP2Bus_Mst_Addr signal.
-//   */
-//  Xil_Out32(BaseAddress+DMA_CONTROLLER_MST_ADDR_REG_OFFSET, DstAddress);
-//
-//  /*
-//   * Set user logic master byte enable register to drive IP2Bus_Mst_BE signal.
-//   */
-//  Xil_Out16(BaseAddress+DMA_CONTROLLER_MST_BE_REG_OFFSET, 0xFFFF);
-//
-//  /*
-//   * Start user logic master write transfer by writting special pattern to its go port.
-//   */
-//  Xil_Out8(BaseAddress+DMA_CONTROLLER_MST_GO_PORT_OFFSET, MST_START);
-//}
-//
-//void DMA_CONTROLLER_MasterRecvWord(Xuint32 BaseAddress, Xuint32 SrcAddress)
-//{
-//  /*
-//   * Set user logic master control register for read transfer.
-//   */
-//  Xil_Out8(BaseAddress+DMA_CONTROLLER_MST_CNTL_REG_OFFSET, MST_SGRD);
-//
-//  /*
-//   * Set user logic master address register to drive IP2Bus_Mst_Addr signal.
-//   */
-//  Xil_Out32(BaseAddress+DMA_CONTROLLER_MST_ADDR_REG_OFFSET, SrcAddress);
-//
-//  /*
-//   * Set user logic master byte enable register to drive IP2Bus_Mst_BE signal.
-//   */
-//  Xil_Out16(BaseAddress+DMA_CONTROLLER_MST_BE_REG_OFFSET, 0xFFFF);
-//
-//  /*
-//   * Start user logic master read transfer by writting special pattern to its go port.
-//   */
-//  Xil_Out8(BaseAddress+DMA_CONTROLLER_MST_GO_PORT_OFFSET, MST_START);
-//}
-
 void DMA_CONTROLLER_CopyData(Xuint32 BaseAddress, Xuint32 SrcBaseAddr, Xuint32 DestBaseAddr, Xuint32 Length)
 {
+  if (Length == 0) {
+    xil_printf("Length should be greater than zero.\n\r");
+    return;
+  }
 	// Write Source
 	DMA_CONTROLLER_mWriteSlaveReg0(BaseAddress, 0, SrcBaseAddr);
 
